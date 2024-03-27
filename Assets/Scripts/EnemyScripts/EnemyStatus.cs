da26d394b89d1eef584d514ef444f65f3cf41e19
brunoathayde <142446897+Onurbmastergit@users.noreply.github.com>
2024-03-27 19:33:05 -0300

    HudLifeZombieAdd

--- Assets/Scripts/EnemyScripts/EnemyStatus.cs
+++ Assets/Scripts/EnemyScripts/EnemyStatus.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class EnemyStatus : MonoBehaviour
 {
@@ -9,7 +12,13 @@ public class EnemyStatus : MonoBehaviour
     public int level = 0;
     public bool boss;
 
+    Color32 corVermelho = new Color32(249, 6, 0, 255); // Red = F90600
+    Color32 corVerde = new Color32(0, 249, 22, 255);   // Green = 00F916
+    Color32 corAmarelo = new Color32(249, 192, 0, 255);// Yellow = F9C000
+
     public AnimationEnemy AnimationEnemy;
+    public UnityEngine.UI.Image lifeBarStatus;
+    public TextMeshProUGUI levelZumbi;
 
     private void Start()
     {
@@ -31,6 +40,24 @@ public class EnemyStatus : MonoBehaviour
         }
         AnimationEnemy = GetComponent<AnimationEnemy>();
         vidaAtual = vidaBase;
+    }
+    private void Update()
+    {
+        levelZumbi.text = "Lvl " + level.ToString(); 
+        float fillAmount = (float)vidaAtual/vidaBase;
+        lifeBarStatus.fillAmount = fillAmount;
+        if (vidaAtual <= vidaBase * 0.2f) // Menos de 20% de vida (Vermelho)
+        {
+            lifeBarStatus.color = corVermelho;
+        }
+        else if (vidaAtual >= vidaBase * 0.8f) // Mais de 80% de vida (Verde)
+        {
+            lifeBarStatus.color = corVerde;
+        }
+        else // Entre 20% e 80% de vida (Amarelo)
+        {
+            lifeBarStatus.color = corAmarelo;
+        }
     }
     public void ReceberDano(int valor) 
     {
